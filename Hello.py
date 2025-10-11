80fce22cb49f4c1933be4e34866dcaf85636581a
Jira2004 <Abigailpoku25@gmail.com>
2025-10-10 23:31:37 -0400

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -3,3 +3,4 @@ name = input("What's your name? ")
 print(f"Nice to meet you, {name}!")
 #display phase "Hello from my GitHub repo!" and will display user name after getting input through a promt# Chris
 print("This is a collaborative change!")# made from Abby
+print("Collaborator edit!"
