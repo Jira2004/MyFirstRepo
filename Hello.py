6cef72e38e47c950e1b643f36afb1b9508c819fb
Chriswya <calcindor@albany.edu>
2025-10-10 23:09:47 -0400

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,1 +1,5 @@
-print("Hello, GitHub!")
+print("Hello from my GitHub repo!")
+name = input("What's your name? ")
+print(f"Nice to meet you, {name}!")
+#display phase "Hello from my GitHub repo!" and will display user name after getting input through a promt# Chris
+print("This is a collaborative change!")# made from Abby
